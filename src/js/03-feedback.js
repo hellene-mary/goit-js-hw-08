1b75e78898a008a4835c967f879df5dbdf0e9962
Grek Maria <grekmariaa@gmail.com>
2022-08-18 19:01:51 +0300

    task-03 is done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+form.addEventListener('submit', throttle(onSubmitClick, 500));
+form.addEventListener('input', inputIracking);
+
+checkStorage();
+
+function inputIracking(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitClick(event) {
+    event.preventDefault();
+
+    console.log(`formData: `, JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function checkStorage() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+        const formDataParse = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        form.elements.email.value = formDataParse.email;
+        form.elements.message.value = formDataParse.message;
+    }
+}
